06bf65e93bbb6b172f8577c1850701400ad56991
Stefan Kaes <stefan.kaes@xing.com>
2013-05-17 18:05:18 +0200

    compatibility with older rails versions

--- lib/logjam_agent/rack/logger.rb
+++ lib/logjam_agent/rack/logger.rb
@@ -121,22 +121,65 @@ require 'action_controller/log_subscriber'
 module ActionController #:nodoc:
 
   class LogSubscriber
-    def start_processing(event)
-      payload = event.payload
-      params  = payload[:params].except(*INTERNAL_PARAMS)
-      format  = payload[:format]
-      format  = format.to_s.upcase if format.is_a?(Symbol)
+    if Rails::VERSION::STRING =~ /^3.0/
+      def start_processing(event)
+        payload = event.payload
+        params  = payload[:params].except(*INTERNAL_PARAMS)
 
-      controller = payload[:controller]
-      action = payload[:action]
-      full_name = "#{controller}##{action}"
-      action_name = LogjamAgent.action_name_proc.call(full_name)
+        controller = payload[:controller]
+        action = payload[:action]
+        full_name = "#{controller}##{action}"
+        action_name = LogjamAgent.action_name_proc.call(full_name)
 
-      # puts "setting logjam action to #{action_name}"
-      Rails.logger.request.fields[:action] = action_name
+        # puts "setting logjam action to #{action_name}"
+        Rails.logger.request.fields[:action] = action_name
 
-      info "Processing by #{full_name} as #{format}"
-      info "  Parameters: #{params.inspect}" unless params.empty?
+        info "  Processing by #{full_name} as #{payload[:formats].first.to_s.upcase}"
+        info "  Parameters: #{params.inspect}" unless params.empty?
+      end
+
+    elsif Rails::VERSION::STRING =~ /^3.1/
+
+      def start_processing(event)
+        payload = event.payload
+        params  = payload[:params].except(*INTERNAL_PARAMS)
+        format  = payload[:format]
+        format  = format.to_s.upcase if format.is_a?(Symbol)
+
+        controller = payload[:controller]
+        action = payload[:action]
+        full_name = "#{controller}##{action}"
+        action_name = LogjamAgent.action_name_proc.call(full_name)
+
+        # puts "setting logjam action to #{action_name}"
+        Rails.logger.request.fields[:action] = action_name
+
+        info "  Processing by #{full_name} as #{format}"
+        info "  Parameters: #{params.inspect}" unless params.empty?
+      end
+
+    elsif Rails::VERSION::STRING =~ /^3.2/
+
+      def start_processing(event)
+        payload = event.payload
+        params  = payload[:params].except(*INTERNAL_PARAMS)
+        format  = payload[:format]
+        format  = format.to_s.upcase if format.is_a?(Symbol)
+
+        controller = payload[:controller]
+        action = payload[:action]
+        full_name = "#{controller}##{action}"
+        action_name = LogjamAgent.action_name_proc.call(full_name)
+
+        # puts "setting logjam action to #{action_name}"
+        Rails.logger.request.fields[:action] = action_name
+
+        info "Processing by #{full_name} as #{format}"
+        info "  Parameters: #{params.inspect}" unless params.empty?
+      end
+
+    else
+      raise "loggjam_agent ActionController monkey patch is not compatible with your Rails version"
     end
   end
 
