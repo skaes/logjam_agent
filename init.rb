959fec479b5f872a708bd287a88d93fe5226bc43
Stefan Kaes <stefan.kaes@xing.com>
2013-07-28 09:49:23 +0200

    lose remnants of rails 2

--- init.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Dispatcher.middleware.insert_before(ActionController::Failsafe, LogjamAgent::Middleware)
-LogjamAgent.application_name ||= "rails"
-LogjamAgent.environment_name ||= Rails.env
