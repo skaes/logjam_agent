77d8948336d6e17c868c4e6b7bbe64e6e168bb68
Stefan Kaes <stefan.kaes@xing.com>
2014-11-08 12:28:02 +0100

    use new meta_info format for messages

--- lib/logjam_agent/util.rb
+++ lib/logjam_agent/util.rb
@@ -5,6 +5,10 @@ module LogjamAgent
 
     UINT64 = "Q"
 
+    META_INFO_VERSION = 1
+    META_INFO_TAG = 0xcabd
+    META_INFO_DEVICE_NUMBER = 0
+
     # we assume we're running on MRI ruby
     FIXNUM_MAX = 2 ** (1.size * 8 - 2) - 1
 
@@ -39,17 +43,19 @@ module LogjamAgent
     end
 
     def pack_info(n)
-      info = pack_uint64_big_endian(zclock_time)
+      info = [META_INFO_TAG, META_INFO_VERSION, META_INFO_DEVICE_NUMBER].pack("nnN")
+      info << pack_uint64_big_endian(zclock_time)
       info << pack_uint64_big_endian(n)
     end
 
     def unpack_info(info)
-      zclock = unpack_uint64_big_endian(info[0..7])
+      tag, version, device = info[0..7].unpack("nnN")
+      zclock = unpack_uint64_big_endian(info[8..15])
       secs = zclock / 1000
       msecs = zclock % 1000
       sent = Time.at(secs) + 1000.0/msecs
-      sequence = unpack_uint64_big_endian(info[8..15])
-      [sent, sequence]
+      sequence = unpack_uint64_big_endian(info[16..23])
+      [tag, version, device, sent, sequence]
     end
   end
 end
