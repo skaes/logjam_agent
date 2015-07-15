528b2db970639609f3510fff696525bd15c4ca6a
Stefan Kaes <stefan.kaes@xing.com>
2015-07-15 16:01:47 +0200

    Revert "set frame_max option for amqp connection"
    This reverts commit 305609b4b4bc25332338097037c256a26d52aca7.

--- lib/logjam_agent/amqp_forwarder.rb
+++ lib/logjam_agent/amqp_forwarder.rb
@@ -95,13 +95,7 @@ module LogjamAgent
 
     #TODO: verify socket_timout for ruby 1.9
     def bunny
-      @bunny ||= Bunny.new(:host => @config[:host],
-                           :socket_timeout => 1.0,
-                           :frame_max => max_frame_size)
-    end
-
-    def max_frame_size
-      (LogjamAgent.max_bytes_all_lines * 1.2).ceil
+      @bunny ||= Bunny.new(:host => @config[:host], :socket_timeout => 1.0)
     end
 
     def ensure_bunny_gem_is_available
