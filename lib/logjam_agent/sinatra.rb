988ce1d63233deb74e96e8e3ed7c652abe6a203e
Stefan Kaes <stefan.kaes@xing.com>
2020-06-25 18:42:29 +0200

    handle logjam agent error logs for Sinatra

--- lib/logjam_agent/sinatra.rb
+++ lib/logjam_agent/sinatra.rb
@@ -18,8 +18,8 @@ module Sinatra
     end
 
     def setup_logjam_logger
-      logfile = settings.respond_to?(:logfile) ? settings.logfile : "#{settings.root}/#{settings.environment}.log"
-      logger = LogjamAgent::BufferedLogger.new(logfile)
+      log_path = ENV["APP_LOG_TO_STDOUT"].present? ? STDOUT : "#{settings.root}/log/#{LogjamAgent.environment_name}.log"
+      logger = LogjamAgent::BufferedLogger.new(log_path)
       loglevel = settings.respond_to?(:loglevel) ? settings.loglevel : :info
       logger.level = ::Logger.const_get(loglevel.to_s.upcase)
       LogjamAgent.log_device_log_level = logger.level
@@ -27,6 +27,17 @@ module Sinatra
       logger = ActiveSupport::TaggedLogging.new(logger)
       LogjamAgent.logger = logger
       ActiveSupport::LogSubscriber.logger = logger
+
+      # install a default error handler for forwarding errors
+      log_path = ENV["APP_LOG_TO_STDOUT"].present? ? STDOUT : "#{settings.root}/log/logjam_agent_error.log"
+      begin
+        forwarding_error_logger = ::Logger.new(log_path)
+      rescue StandardError
+        forwarding_error_logger = ::Logger.new(STDERR)
+      end
+      forwarding_error_logger.level = ::Logger::ERROR
+      forwarding_error_logger.formatter = ::Logger::Formatter.new
+      LogjamAgent.forwarding_error_logger = forwarding_error_logger
     end
 
     def self.registered(app)
@@ -35,7 +46,8 @@ module Sinatra
       app.use LogjamAgent::Middleware, :sinatra
       app.use LogjamAgent::Rack::Logger
 
-      LogjamAgent.environment_name = app.settings.environment
+      LogjamAgent.environment_name = ENV['LOGJAM_ENV'] || ENV['APP_ENV'] || app.settings.environment.to_s
+      LogjamAgent.auto_detect_logged_exceptions
 
       app.enable :logging
     end
