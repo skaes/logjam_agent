d9b2b971a39ec4034cf0ff8e7eba573164a7150c
Stefan Kaes <stefan.kaes@xing.com>
2020-06-25 12:33:48 +0200

    test classic Sinatra app support

---
+++ test/sinatra_classic_app.rb
@@ -0,0 +1,29 @@
+$:.unshift File.expand_path('../../lib', __FILE__)
+
+require 'logjam_agent/sinatra'
+
+configure do
+  set :root, File.expand_path('../..', __FILE__)
+  set :environment, :test
+  set :loglevel, :debug
+  setup_logjam_logger
+
+  LogjamAgent.application_name = "myapp"
+  LogjamAgent.add_forwarder(
+    :zmq,
+    :host => "localhost",
+    :port => 9604,
+    :linger     => 10,
+    :snd_hwm    => 10,
+    :rcv_hwm    => 10,
+    :rcv_timeo  => 10,
+    :snd_timeo  => 10
+  )
+  LogjamAgent.parameter_filters << :password
+end
+
+get '/index' do
+  action_name "Simple#index"
+  logger.info 'Hello World!'
+  'Hello World!'
+end
---
+++ test/sinatra_classic_test.rb
@@ -0,0 +1,20 @@
+require_relative "test_helper.rb"
+require_relative "sinatra_classic_app"
+require "rack/test"
+
+module LogjamAgent
+  class SinatraClassicTest < MiniTest::Test
+
+    include ::Rack::Test::Methods
+
+    def app
+      Sinatra::Application
+    end
+
+    def test_root
+      get '/index?mumu=1&password=5'
+      assert_equal 'Hello World!', last_response.body
+    end
+
+  end
+end
