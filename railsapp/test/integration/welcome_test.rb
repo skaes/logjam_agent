4c8dba9446e54fe76414e014f2d3f58649106549
Stefan Kaes <stefan.kaes@xing.com>
2021-08-06 12:47:21 +0200

    adapted welcome controller test for different rails versions

--- railsapp/test/integration/welcome_test.rb
+++ railsapp/test/integration/welcome_test.rb
@@ -24,15 +24,24 @@ class WelcomeTest < ActionDispatch::IntegrationTest
     assert_kind_of String, payload["ip"]
     assert_kind_of Float, payload["view_time"]
     lines = payload["lines"]
+    puts "Rails::VERSION::STRING: #{Rails::VERSION::STRING}"
+    lines.each{|l|puts l[2]}
     assert_match(/Started GET "\/\?password=\[FILTERED\]"/, lines[0][2])
     assert_match(/Processing by WelcomeController#index/, lines[1][2])
     assert_match(/Parameters.*{"password"=>"\[FILTERED\]"/, lines[2][2])
-    assert_match(/Rendering/, lines[3][2])
-    assert_match(/Rendering/, lines[4][2])
-    assert_match(/Rendered/, lines[5][2])
-    assert_match(/Rendered/, lines[6][2])
-    assert_match(/Completed 200 OK/, lines[7][2])
-    assert_nil(lines[8])
+    if Gem::Version.new(Rails::VERSION::STRING) >= Gem::Version.new("6.1")
+      assert_match(/Rendering/, lines[3][2])
+      assert_match(/Rendering/, lines[4][2])
+      assert_match(/Rendered/, lines[5][2])
+      assert_match(/Rendered/, lines[6][2])
+      assert_match(/Completed 200 OK/, lines[7][2])
+      assert_nil(lines[8])
+    else
+      assert_match(/Rendering/, lines[3][2])
+      assert_match(/Rendered/, lines[4][2])
+      assert_match(/Completed 200 OK/, lines[5][2])
+      assert_nil(lines[6])
+    end
     request_info = payload["request_info"]
     method, url, query_parameters = request_info.values_at(*%w(method url query_parameters))
     assert_equal method, "GET"
