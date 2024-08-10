ecd40faab05913b97cceee09f4f46d0ba5a04067
Stefan Kaes <stkaes@gmail.com>
2024-08-10 13:44:33 +0200

    fix cache format deprecation warnings in integration test

--- railsapp/config/application.rb
+++ railsapp/config/application.rb
@@ -19,8 +19,8 @@ Bundler.require(*Rails.groups)
 
 module Railsapp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.1
+    # Initialize configuration defaults.
+    config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
